db0a0d55023ff5d35d6ba570a5132663007e831c
Taras Liush <taras.liush@gmail.com>
2021-08-26 20:40:40 +0300

    Main page, header, navigation and categories components added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,59 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+import logo from "../public/Logo.png";
+import Close from "../public/Close.png";
+import Image from "next/image";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  function Header() {
+    const router = useRouter();
+    const lvl = router.pathname;
+    switch (lvl) {
+      case "/":
+        return (
+          <div className={styles.header}>
+            <div></div>
+            <div className={styles.logo}>
+              <Image src={logo} />
+            </div>
+            <div></div>
+          </div>
+        );
+      case "/[...category]":
+        return (
+          <div className={styles.header}>
+            <Link href="/">
+              <a className={styles.closeButton}>{"<"}</a>
+            </Link>
+            <div className={styles.logo}>
+              <Image src={logo} />
+            </div>
+            <div></div>
+          </div>
+        );
+      case 2:
+        return (
+          <div className={styles.header}>
+            <div></div>
+            <Image src={Close} />
+            <div className={styles.logo}>
+              <Image src={logo} />
+            </div>
+            <div></div>
+          </div>
+        );
+      default:
+        return null;
+    }
+  }
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
